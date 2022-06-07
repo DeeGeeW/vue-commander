d1c016688f2ea259cdcd6865e846d41225d8eb87
DeeGeeWest <DeeGeeWest@gmail.com>
2022-06-07 18:20:41 -0400

    trouble with image rendering need to look up how i got through for capstone

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link>
     |
-    <router-link to="/about">About</router-link>
+    <router-link to="/decks">Deck</router-link>
   </nav>
-  <router-view />
 </template>
 
 <style>
---
+++ src/views/DecklistVue.vue
@@ -0,0 +1,19 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      cards: {},
+    };
+  },
+  mounted: function () {
+    console.log(this.$route.params.id);
+    axios.get(`/decks/${this.$route.params.id}`).then((response) => {
+      this.deck = response.data;
+      // this.cards = this.deck.cards;
+      // console.log(this.cards);
+      console.log(this.deck);
+    });
+  },
+};
+</script>
